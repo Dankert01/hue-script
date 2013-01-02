52e64bd791e8ab2299482c0ca38c44fdc47e7ddf
Jay Liu <jayliu50@gmail.com>
2013-01-01 21:03:11 -0700

    Update hue.ps1
    Retrieve the sunrise and sunset times
    Changed the webservice to point to earthtools

--- hue.ps1
+++ hue.ps1
@@ -1,6 +1,16 @@
+#http://www.earthtools.org/sun/<latitude>/<longitude>/<day>/<month>/<timezone>/<dst>
 $today = Get-Date
-$url = "http://aa.usno.navy.mil/cgi-bin/aa_pap.pl"
-$postData = [System.Text.Encoding]::ASCII.GetBytes("xxy=$($today.Year)&xxm=$($today.Month)&xxd=$($today.Day)&st=UT&place=provo")
+$todayDay = $today.Day
+$todayMonth = $today.Month
+$timezone = "-7"
+$lat = "40.655234" 
+$long = "-111.882368"
+$daylightSavings = "0"
+$url = "http://www.earthtools.org/sun/$lat/$long/$todayDay/$todayMonth/$timezone/$daylightSavings"
+
+# form the web request
+# see http://www.earthtools.org/webservices.htm#sun
+$postData = [System.Text.Encoding]::ASCII.GetBytes($url)
 $webRequest = [System.Net.WebRequest]::Create($url)
 $webRequest.Method = "POST"
 $webRequest.ContentLength = $postData.Length
@@ -9,9 +19,18 @@ $requestStream = $webRequest.GetRequestStream()
 $requestStream.Write($postData, 0, $postData.Length)
 $requestStream.Close()
 $streamReader = New-Object System.IO.Streamreader -ArgumentList $webRequest.GetResponse().GetResponseStream()
-$response = $streamReader.ReadToEnd()
-$response > "times.txt"
-$streamReader.Close()
+$response = [xml]$streamReader.ReadToEnd()
+
+# obtain the data
+$prepend = $today.ToString("yyyy-MM-dd") + "T"
+$sunrise = $prepend + $response.sun.morning.sunrise
+$mcTwilight = $prepend + $response.sun.morning.twilight.civil
+$maTwilight = $prepend + $response.sun.morning.twilight.astronomical
+$sunset = $prepend + $response.sun.evening.sunset
+$ecTwilight = $prepend + $response.sun.evening.twilight.civil
+$eaTwilight = $prepend + $response.sun.evening.twilight.astronomical
+
+
 
 $hueBridgeIP = "192.168.1.134"
 $allowedHash = "71a6b6f094346a8832df801c8428ea06"
